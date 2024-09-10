81b2f11718065f475bd6281f67aac89b07894ea8
Seth Martins <seth.martins@gmail.com>
2024-09-10 01:13:22 -0300

    entregando o desafio

--- Program.cs
+++ Program.cs
@@ -16,10 +16,19 @@ hospedes.Add(p2);
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+Reserva reserva = new Reserva(diasReservados: 10);
+try{
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+    reserva.CadastrarSuite(suite);
+    reserva.CadastrarHospedes(hospedes);
+    
+
+    // Exibe a quantidade de hóspedes e o valor da diária
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+
+}catch(Exception ex)
+{
+    Console.WriteLine($"A capacide maxima da suite é: {suite.Capacidade} {ex.Message}");
+}
